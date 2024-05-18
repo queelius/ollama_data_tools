a446db2df5b1df7f47ecb6e1fd5e63fc7cc7120b
queelius <lex@metafunctor.com>
2024-05-18 07:45:38 -0500

    updates

--- test.sh
+++
@@ -1,6 +0,0 @@
-../llm/llamagpu \
-	--model ../models/mistral_latest.gguf \
-	--prompt prompt.txt \
-	--n_predict "[INST] You are a helpful assistant. [/INST]" \
-	--stop "[INST]" \
-	--stop "[/INST]"
